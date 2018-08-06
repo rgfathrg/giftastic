3d667fecd44b59dd23d15143a11954847196bb2e
rgfathrg <rgfath@gmail.com>
2018-08-05 21:15:11 -0400

    fass

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -12,14 +12,14 @@ $(document).ready(function() {
             console.log(bId.val());
             console.log(bCount);
         }
-    }
+    }//Closes out pubButtons function
 
     $("#b-add").on("click", function(event) {
         event.preventDefault();
         gifCats.push($("#bInput").val());
         $("#bInput").val("");
         pubButtons();
-    });
+    });//Closes out the #b-add function click event
     
     pubButtons();
 
@@ -42,22 +42,44 @@ $(document).ready(function() {
           var getData = response.data;
           $("#gifPlace").empty();
           console.log(getData);
+        
           for (var i = 0; i < getData.length; i++) {
               var gifDiv = $("<div>");
 
               var ratings = getData[i].rating;
               var p = $("<p>").text("Ratings: " + ratings);
-
+              
               var $img = $("<img>");
-              $img.attr("src", getData[i].images.fixed_height.url);
-
+              $img.attr("src", getData[i].images.fixed_height_still.url).attr("data-animate", getData[i].images.fixed_height.url).attr("data-still", getData[i].images.fixed_height_still.url).attr("class", "picClick").attr("data-state", "still");
+              
+              
+            console.log($img);
               gifDiv.prepend(p);
               gifDiv.prepend($img);
+              
 
               $("#gifPlace").prepend(gifDiv);
+              
+              
+            }
 
-          }
+          
+            $(document.body).on("click", ".picClick", function() {
+                
+                if ($(this).attr("data-state") === "still") {
+                    $(this).attr("src", $(this).attr("data-animate"));
+                    $(this).attr("data-state", "animate");
+                    
+                }
+                else {
+                    $(this).attr("src", $(this).attr("data-still"));
+                    $(this).attr("data-state", "still");
+                }
+
+            });
+        
         });
-
     });
 });
+
+
